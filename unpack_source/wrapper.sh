540cc9cab9665e90c65d93deddb4813e208f1302
Maarten ter Huurne <maarten@treewalker.org>
2019-09-14 06:48:21 +0200

    unpack_source: Add wrapper to unpack source tarball

---
+++ unpack_source/wrapper.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+exit_with_error() {
+    echo "Aborting build because of error: $1"
+    echo "result=error" >> "$SF_RESULTS"
+    echo "summary=$1" >> "$SF_RESULTS"
+    exit 0
+}
+
+# Create and enter source directory.
+UNPACK_DIR="$SF_PRODUCT_ROOT/unpacked_source"
+mkdir -p "$UNPACK_DIR"
+cd "$UNPACK_DIR" || exit_with_error "Failed to enter unpack directory $UNPACK_DIR"
+
+# Unpack tarball.
+tar zxvf "$SF_PRODUCT_ROOT/$OPENMSX_SRC_PACKAGE" || exit_with_error "Failed to unpack sources"
+
+# Verify that the expected source directory exists.
+SOURCE_DIR="$UNPACK_DIR"/`basename "$OPENMSX_SRC_PACKAGE" .tar.gz`
+if [ ! -d "$SOURCE_DIR" ]
+then
+    exit_with_error "Expected source directory $SOURCE_DIR was not created"
+fi
+
+# Write results file.
+echo "result=ok" > "$SF_RESULTS"
+echo "summary=Sources unpacked" >> "$SF_RESULTS"
+echo "output.$SF_OUTPUTS.locator=$SOURCE_DIR" >> "$SF_RESULTS"
